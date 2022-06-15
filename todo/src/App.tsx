e77a619642b5ac0087ba95add9db79ad27a648ee
lrnholanda <lrnholanda@gmail.com>
2022-06-15 14:47:12 -0300

    todo task project in typescript

---
+++ todo/src/App.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import './App.css';
+import InputFeild from './components/InputFeild';
+
+
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  return (
+    <div className="App">
+     <span className="heading">Taskify</span>
+     <InputFeild todo= {todo} setTodo= {setTodo} />
+    </div>
+  );
+}
+
+export default App;
